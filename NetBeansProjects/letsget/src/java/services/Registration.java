354a2c45e6b43ef25b3ae50429f844b89414ed08
andrewjacobsen <devbeanaj@gmail.com>
2017-10-31 18:51:27 -0400

    Finalized registration page, created response email and added accountID.

--- NetBeansProjects/letsget/src/java/services/Registration.java
+++ NetBeansProjects/letsget/src/java/services/Registration.java
@@ -3,6 +3,9 @@ package services;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -29,30 +32,37 @@ public class Registration extends HttpServlet {
         pass = context.getInitParameter("pass");
     }
    
-   static String convertStreamToString(java.io.InputStream is) {
-    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-    return s.hasNext() ? s.next() : "";
-    }
-   
-
    public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws IOException, ServletException {
  
+      //gather all registration information
+      String name = request.getParameter("name");
+      String email = request.getParameter("email");
+      String phone = request.getParameter("phone");
+      String street = request.getParameter("street");
+      String city = request.getParameter("city");
+      String zip = request.getParameter("zip");
+      String state = request.getParameter("state");
+      String card = request.getParameter("card");
+      String exp = request.getParameter("exp");
+      String type = request.getParameter("type");
+      String password1 = request.getParameter("password1");
+      String code = UUID.randomUUID().toString();
+      //get registration date for generating unique account id
+      DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+      Date date = new Date();
+      String regDate= dateFormat.format(date);
+      //Generate unique account ID using registration date and first initials
+      String accountID= name.substring(0, 3) + regDate;
+      //note:: Might be worthwhile to check to see if the id is used, although very low chance of this.
+      
       // Set the response MIME type of the response message
       response.setContentType("text/html");
       // Allocate a output writer to write the response message into the network socket
-      String name = request.getParameter("name");
-      String email = request.getParameter("email");
-      String password1 = request.getParameter("password1");
-      String code = UUID.randomUUID().toString();
-      
-      String subject = "Registration Confirmation";
       PrintWriter out = response.getWriter();
-      InputStream registrationPage  = getClass().getResourceAsStream("/services/resources/regresponse.html");
-      String message = convertStreamToString(registrationPage);
-      message += "Name: " + name;
-      message += "<br>Email: " + email;
-      message += "<br><br>Confirmation Code: " + code;
+      //Generate response email
+      String subject = "Welcome! Registration Confirmation";
+      String message = createConfirmationEmail(name, email, phone, street, city, zip, state, card, exp, type, code, accountID);
       //generate confirmation code
       
               
@@ -78,5 +88,239 @@ public class Registration extends HttpServlet {
       } finally {
          out.close();  // Always close the output writer
       }
+
+   }
+   
+   static String convertStreamToString(java.io.InputStream is) {
+    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+    }
+   
+   private String createConfirmationEmail(String name, String email, String phone, String street, String city,
+           String zip, String state, String card, String exp, String type, String code, String accountID){
+           //generate an HTML response email.
+           String msg = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+            "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            " <head>\n" +
+            "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
+            "  <title>Registration Confirmation</title>\n" +
+            "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n" +
+            "</head>\n" +
+            "    \n" +
+            "<body style=\"margin: 0; padding: 0;\">\n" +
+            " <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "  <tr>\n" +
+            "   <td>\n" +
+            "    <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border-collapse: collapse;\">\n" +
+            "    \n" +
+            "    <tr>\n" +
+            "      <td>\n" +
+            "          <b>Welcome to Let's Get Thrifty, below are your confirmation details.</b><br><br>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "    <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Name:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            name +
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "    <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Email:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            email +
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Phone:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            phone +
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Street:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            street+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           City:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            city+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           State:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            state+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "       <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Card:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            card+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Expiration:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            exp+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           Type:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            type+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            "           accountID:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\">\n" +
+            accountID+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "        <tr>\n" +
+            "      <td>\n" +
+            "       \n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "     <tr>\n" +
+            "        <tr>\n" +
+            "      <td>\n" +
+            "       <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+            "         <tr>\n" +
+            "          <td width=\"260\" valign=\"top\"><br>\n" +
+            "           Confirmation Code:\n" +
+            "          </td>\n" +
+            "          <td style=\"font-size: 0; line-height: 0;\" width=\"20\">\n" +
+            "           &nbsp;\n" +
+            "          </td>\n" +
+            "          <td width=\"260\" valign=\"top\"><br>\n" +
+            code+
+            "          </td>\n" +
+            "         </tr>\n" +
+            "        </table>\n" +
+            "      </td>\n" +
+            "     </tr>\n" +
+            "    </table>\n" +
+            "   </td>\n" +
+            "  </tr>\n" +
+            " </table>\n" +
+            "</body>\n" +
+            "</html>";
+       return msg;
    }
 }
