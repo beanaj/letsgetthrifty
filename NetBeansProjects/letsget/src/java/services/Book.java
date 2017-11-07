60aa4681c0048f54e8d61151341b5e10e4d8f674
Addison Powers <afp95694@uga.edu>
2017-11-06 22:02:19 -0500

    Added Book.java and BookDAO.java

---
+++ NetBeansProjects/letsget/src/java/services/Book.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Addison
+ */
+
+package services;
+
+public class Book {
+   private String isbn;
+   private String genre;
+   private String author;
+   private String title;
+   private double rating;
+   private String picture;
+   private int edition;
+   private String publisher;
+   private int publicationYear;
+   private int qtyInStock;
+   private int minThreshold;
+   private double buyPrice;
+   private double sellPrice;
+   private int supplierID;
+   
+   public String getISBN() {
+       return this.isbn;
+   }
+   public void setISBN(String str) {
+       this.isbn = str;
+   }
+   public String getGenre() {
+       return this.genre;
+   }
+   public void setGenre(String str) {
+       this.genre = str;
+   }
+   public String getAuthor() {
+       return this.author;
+   }
+   public void setAuthor(String str) {
+       this.author = str;
+   }
+   public String getTitle() {
+       return this.title;
+   }
+   public void setTitle(String str) {
+       this.title = str;
+   }
+   public Double getRating() {
+       return this.rating;
+   }
+   public void setRating(int rat) {
+       this.rating = rat;
+   }
+   public String getPicture() {
+       return this.picture;
+   }
+   public void setPicture(String str) {
+       this.picture = str;
+   }
+   public int getEdition() {
+       return this.edition;
+   }
+   public void setEdition(int edition) {
+       this.edition = edition;
+   }
+   public String getPublisher() {
+       return this.publisher;
+   }
+   public void setPublisher(String str) {
+       this.publisher = str;
+   }
+   public int getPublicationYear() {
+       return this.publicationYear;
+   }
+   public void setPublicationYear(int pYear) {
+       this.publicationYear = pYear;
+   }
+   public int getQtyInStock() {
+       return this.qtyInStock;
+   }
+   public void setQtyInStock(int qty) {
+       this.qtyInStock = qty;
+   }
+   public int getMinThreshold() {
+       return this.minThreshold;
+   }
+   public void setMinThreshold(int minThresh) {
+       this.minThreshold = minThresh;
+   }
+   public double getBuyPrice() {
+       return this.buyPrice;
+   }
+   public void setBuyPrice(double buyP) {
+       this.buyPrice = buyP;
+   }
+   public double getSellPrice() {
+       return this.sellPrice;
+   }
+   public void setSellPrice(double sellP) {
+       this.sellPrice = sellP;
+   }
+   public int getSupplierID() {
+       return this.supplierID;
+   }
+   public void setSupplierID(int supID) {
+       this.supplierID = supID;
+   }
+}
---
+++ NetBeansProjects/letsget/src/java/services/BookDAO.java
@@ -0,0 +1,68 @@
+
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Addison
+ */
+public class BookDAO {
+    private DataSource dataSource;
+    
+//    public BookDAO(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    
+    public List<Book> list() throws SQLException {
+        DatabaseUtility db = new DatabaseUtility();
+        
+        List<Book> books = new ArrayList<Book>();
+        
+        try {
+            Class.forName(db.getDriver());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        try (
+            Connection connection = DriverManager.getConnection(db.getURL(), db.getUser(), db.getPass());
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM books");
+            ResultSet rs = statement.executeQuery();
+                ) {
+            while (rs.next()) {
+                Book book = new Book();
+                book.setAuthor(rs.getString("author"));
+                book.setBuyPrice(rs.getDouble("buyPrice"));
+                book.setEdition(rs.getInt("edition"));
+                book.setGenre(rs.getString("genre"));
+                book.setISBN(rs.getString("isbn"));
+                book.setMinThreshold(rs.getInt("minThreshold"));
+                book.setPicture(rs.getString("picture"));
+                book.setPublicationYear(rs.getInt("publicationYear"));
+                book.setPublisher(rs.getString("publisher"));
+                book.setQtyInStock(rs.getInt("qtyInStock"));
+                book.setRating(rs.getInt("rating"));
+                book.setSellPrice(rs.getDouble("sellPrice"));
+                book.setSupplierID(rs.getInt("supplierID"));
+                book.setTitle(rs.getString("title"));
+            }
+        
+        }
+        return books;
+    }
+}
+
