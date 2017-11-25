2cf2b6c592133f59cd40a5463b04df6b605b70a6
Addison Powers <afp95694@uga.edu>
2017-11-25 16:16:25 -0500

    Order and OrderDAO classes implemented

---
+++ NetBeansProjects/letsget/src/java/entity/Order.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Addison
+ */
+public class Order {
+    private int orderID;
+    private int shippingAgencyID;
+    private String orderStatus;
+    private String orderDate;
+    private String shippingAdd;
+    private String billingAdd;
+    private String paymentMethod;
+    private int confNum;
+    private int transID;
+    private String userID;
+    private String orderTotal;
+    private String cCardID;
+    
+    //Constructor:
+    public Order(int oID, int sAID, String oStat, String oDate, String sAdd, String bAdd, String pay, int conf, int trans, String user, String orderT, String cCard) {
+        this.orderID = oID;
+        this.shippingAgencyID = sAID;
+        this.orderStatus = oStat;
+        this.orderDate = oDate;
+        this.shippingAdd = sAdd;
+        this.billingAdd = bAdd;
+        this.paymentMethod = pay;
+        this.confNum = conf;
+        this.transID = trans;
+        this.userID = user;
+        this.orderTotal = orderT;
+        this.cCardID = cCard;
+    }
+    
+    public Order() {
+        
+    }
+    
+    public void setOrderID(int oID) {
+        this.orderID = oID;
+    }
+    public int getOrderID() {
+        return this.orderID;
+    }
+    public void setShippingAgencyID(int sAID) {
+        this.orderID = sAID;
+    }
+    public int getShippingAgencyID() {
+        return this.shippingAgencyID;
+    }
+    public void setOrderStatus(String orderStat) {
+        this.orderStatus = orderStat;
+    }
+    public String getOrderStatus() {
+        return this.orderStatus;
+    }
+    public void setOrderDate(String date) {
+        this.orderDate = date;
+    }
+    public String getOrderDate() {
+        return this.orderDate;
+    }
+    public void setShippingAddress(String shipAdd) {
+        this.shippingAdd = shipAdd;
+    }
+    public String getShippingAddress() {
+        return this.shippingAdd;
+    }
+    public void setBillingAddress(String add) {
+        this.billingAdd = add;
+    }
+    public String getBillingAddress() {
+        return this.billingAdd;
+    }
+    public void setPaymentMethod(String pay) {
+        this.paymentMethod = pay;
+    }
+    public String getPaymentMethod() {
+        return this.paymentMethod;
+    }
+    public void setConfirmationNumber(int cNum) {
+        this.confNum = cNum;
+    }
+    public int getConfirmationNumber() {
+        return this.confNum;
+    }
+    public void setTransactionID(int tID) {
+        this.transID = tID;
+    }
+    public int getTransactionID() {
+        return this.transID;
+    }
+    public void setUserID(String uID) {
+        this.userID = uID;
+    }
+    public String getUserID() {
+        return this.userID;
+    }
+    public void setOrderTotal(String oT) {
+        this.orderTotal = oT;
+    }
+    public String getOrderTotal() {
+        return this.orderTotal;
+    }
+    public void setCreditCardID(String cC) {
+        this.cCardID = cC;
+    }
+    public String getCreditCardID() {
+        return this.cCardID;
+    }
+}
---
+++ NetBeansProjects/letsget/src/java/entity/OrderDAO.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import services.DatabaseUtility;
+
+/**
+ *
+ * @author Addison
+ */
+public class OrderDAO {
+    public List<Order> list() throws SQLException {
+        DatabaseUtility db = new DatabaseUtility();
+        
+        List<Order> orders = new ArrayList<Order>();
+        
+        try {
+            Class.forName(db.getDriver());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        try (
+            Connection connection = DriverManager.getConnection(db.getURL(), db.getUser(), db.getPass());
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM orders");
+            ResultSet rs = statement.executeQuery();
+                ) {
+            while (rs.next()) {
+                Order order = new Order();
+                order.setOrderID(rs.getInt("orderID"));
+                order.setShippingAgencyID(rs.getInt("shippingAgencyID"));
+                order.setOrderStatus(rs.getString("orderStatus"));
+                order.setOrderDate(rs.getString("orderDate"));
+                order.setShippingAddress(rs.getString("shippingAddress"));
+                order.setBillingAddress(rs.getString("billingAdress"));
+                order.setPaymentMethod(rs.getString("paymentMethod"));
+                order.setConfirmationNumber(rs.getInt("confirmationNumber"));
+                order.setTransactionID(rs.getInt("transactionID"));
+                order.setUserID(rs.getString("userID"));
+                order.setOrderTotal(rs.getString("orderTotal"));
+                order.setCreditCardID(rs.getString("creditCardID"));
+                
+                orders.add(order);
+            }
+        
+        }
+        return orders;
+    }
+    
+    //Add a book to the database
+    public void addOrder(Order o) {
+        //Set up database connection:
+        Connection conn = null;
+        PreparedStatement stat = null;
+        DatabaseUtility db = new DatabaseUtility();
+        try {
+            Class.forName(db.getDriver());
+            conn = DriverManager.getConnection(db.getURL(), db.getUser(), db.getPass());
+            String query = "INSERT INTO orders (orderID, shippingAgencyID, orderStatus, orderDate, shippingAddress, billingAdress, paymentMethod, confirmationNumber, transactionID, userID, orderTotal, creditCardID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            stat = conn.prepareStatement(query);
+            stat.setInt(1, o.getOrderID());
+            stat.setInt(2, o.getShippingAgencyID());
+            stat.setString(3, o.getOrderStatus());
+            stat.setString(4, o.getOrderDate());
+            stat.setString(5, o.getShippingAddress());
+            stat.setString(6, o.getBillingAddress());
+            stat.setString(7, o.getPaymentMethod());
+            stat.setInt(8, o.getConfirmationNumber());
+            stat.setInt(9, o.getTransactionID());
+            stat.setString(10, o.getUserID());
+            stat.setString(11, o.getOrderTotal());
+            stat.setString(12, o.getCreditCardID());
+            stat.execute();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
